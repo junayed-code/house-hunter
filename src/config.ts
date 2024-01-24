98f31b71ea220c8c69b1a34cb63cfbb8d6a5f377
junayed-code <junayed.code@gmail.com>
2024-01-24 23:57:14 +0600

    Create config.js file

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import sliderImg1 from "@/assets/images/slider-01.jpg";
+import sliderImg2 from "@/assets/images/slider-02.jpg";
+
+export const SLIDER_IMAGES = [sliderImg1, sliderImg2];
+
+export default {
+  SLIDER_IMAGES,
+};

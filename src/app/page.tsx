10a958f2e6d7d010b804be364bb1d9fbbbdfdca4
junayed-code <junayed.code@gmail.com>
2024-01-24 23:58:39 +0600

    Update page.jsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import ImageSlider from "@/components/ImageSlider";
+import SearchHouse from "@/components/SearchHouse";
+import { SLIDER_IMAGES } from "@/config";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl font-bold">House Hunter</h1>
-    </div>
+    <main>
+      <div className="relative">
+        <ImageSlider images={SLIDER_IMAGES} />
+        <div className="absolute inset-0 bg-black/40 z-[2]" />
+        <SearchHouse />
+      </div>
+      <p className="mt-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        optio id ut dolore recusandae eius labore facilis maxime, culpa
+        voluptate officiis cum nesciunt itaque harum porro laudantium. Tempore
+        delectus in aliquam recusandae, voluptas tempora, numquam consequuntur
+        inventore sunt aliquid similique soluta pariatur velit, enim animi!
+        Similique eos facere asperiores reprehenderit.
+      </p>
+    </main>
   );
 }

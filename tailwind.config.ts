fdb72f82ce1afd851d2d42c124c75cb004fad7ff
junayed-code <junayed.code@gmail.com>
2024-01-24 23:56:44 +0600

    Update tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1.25rem",
+      screens: {
+        md: "760px",
+        lg: "990px",
+        xl: "1125px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-rubik)"],
